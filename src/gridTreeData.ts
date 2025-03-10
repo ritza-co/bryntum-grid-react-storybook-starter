e73e9af3b724520f25da83084da6ddd24645aa7d
Matthew Clarke <m.d.clarke.sa@gmail.com>
2025-03-10 12:51:17 +0200

    fix: data source

---
+++ src/gridTreeData.ts
@@ -0,0 +1,25 @@
+export const treeData = [
+    {
+        id       : 1,
+        name     : 'ABBA',
+        iconCls  : 'b-icon b-fa-users',
+        born     : null,
+        expanded : true,
+        children : [
+            { id : 11, name : 'Anni-Frid', born : 1945, iconCls : 'b-icon b-fa-user' },
+            { id : 12, name : 'Bjorn', born : 1945, iconCls : 'b-icon b-fa-user' },
+            { id : 13, name : 'Benny', born : 1946, iconCls : 'b-icon b-fa-user' },
+            { id : 14, name : 'Agnetha', born : 1950, iconCls : 'b-icon b-fa-user' }
+        ]
+    },
+    {
+        id       : 2,
+        name     : 'Roxette',
+        iconCls  : 'b-icon b-fa-users',
+        born     : null,
+        children : [
+            { id : 21, name : 'Per', born : 1959, iconCls : 'b-icon b-fa-user' },
+            { id : 22, name : 'Marie', born : 1958, iconCls : 'b-icon b-fa-user' }
+        ]
+    }
+];
